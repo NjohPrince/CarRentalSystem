d357ae00b87f0ce8ba9a01f03881a4702265b04d
NjohPrince <jufredprince@gmail.com>
2021-06-21 01:12:39 +0100

    class Car consructor and attributes

--- model/src/main/Main.java
+++ model/src/main/Main.java
@@ -1,7 +1,23 @@
 package main;
 
+/**
+ * @author ProgrammingGeek - Njoh Noh Prince Junior
+ * @since 21-06-2021
+ *
+ * Car Rent Management System - Demonstrating OOP in Java Programming languages
+ */
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Car Rental System");
+
+        System.out.println("*******************************************");
+        System.out.println("*******************************************");
+        System.out.println("**                                       **");
+        System.out.println("**   WELCOME TO PJ's CAR RENTAL SYSTEM   **");
+        System.out.println("**                                       **");
+        System.out.println("*******************************************");
+        System.out.println("*******************************************");
+
+        System.out.println();
     }
 }
--- model/src/main/model/Car.java
+++ model/src/main/model/Car.java
@@ -1,4 +1,40 @@
 package main.model;
 
+/**
+ * Class for keeping car details, getting information about a car and setting up details
+ */
+
 public class Car {
+
+    private int carId;
+    private String name;
+    private String brand;
+    private String numberPlate;
+    private int rentPricePerDay;
+    private int costPrice;
+    private String color;
+    private Customer currentUser;
+
+    /**
+     *
+     * @param carId
+     * @param name
+     * @param brand
+     * @param numberPlate
+     * @param rentPricePerDay
+     * @param costPrice
+     * @param color
+     */
+
+    public Car(int carId, String name, String brand, String numberPlate, int rentPricePerDay, int costPrice, String color) {
+        this.carId = carId;
+        this.name = name;
+        this.brand = brand;
+        this.numberPlate = numberPlate;
+        this.rentPricePerDay = rentPricePerDay;
+        this.costPrice = costPrice;
+        this.color = color;
+    }
+
+    
 }
