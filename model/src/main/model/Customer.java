4629efe048fe463bac3e3c493463ec8cb1a399f1
NjohPrince <jufredprince@gmail.com>
2021-06-21 01:17:41 +0100

    cutomer contructor alongside getters and setters

--- model/src/main/model/Customer.java
+++ model/src/main/model/Customer.java
@@ -1,4 +1,91 @@
 package main.model;
 
+/**
+ * Class for keeping customer details, getting information about customer and setting up details
+ */
+
 public class Customer {
+
+    private int customerId;
+    private String name;
+    private int age;
+    private String licenseNumber;
+    private String nationalIDNumber;
+
+
+    /**
+     *
+     * @param customerId
+     * @param name
+     * @param age
+     * @param licenseNumber
+     * @param nationalIDNumber
+     */
+    public Customer(int customerId, String name, int age, String licenseNumber, String nationalIDNumber) {
+        this.customerId = customerId;
+        this.name = name;
+        this.age = age;
+        this.licenseNumber = licenseNumber;
+        this.nationalIDNumber = nationalIDNumber;
+
+    }
+
+    /**
+     * Getters and Setters
+     * @return
+     * @setValues
+     */
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getLicenseNumber() {
+        return licenseNumber;
+    }
+
+    public void setLicenseNumber(String licenseNumber) {
+        this.licenseNumber = licenseNumber;
+    }
+
+    public String getNationalIDNumber() {
+        return nationalIDNumber;
+    }
+
+    public void setNationalIDNumber(String nationalIDNumber) {
+        this.nationalIDNumber = nationalIDNumber;
+    }
+
+    /**
+     * Display in a more clear and detailed way
+     * @return
+     */
+    @Override
+    public String toString() {
+        return  "customerId=" + customerId +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", licenseNumber='" + licenseNumber + '\'' +
+                ", nationalIDNumber='" + nationalIDNumber + '\'';
+    }
 }
